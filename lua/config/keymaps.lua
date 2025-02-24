45fafcbf782ce28655bd952c1de3ff5f6ba6cb5b
Zelferion <zelferionedits@gmail.com>
2025-02-24 13:36:24 -0500

    c

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,7 +18,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Keybinds for navigating split windows using Ctrl + arrow keys
 vim.api.nvim_set_keymap('n', '<C-Up>',    '<C-w>k', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-Down>',  '<C-w>j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-Left>',  '<C-w>h', { noremap = true, silent = true })
@@ -26,15 +25,12 @@ vim.api.nvim_set_keymap('n', '<C-Right>', '<C-w>l', { noremap = true, silent = t
 
 local opts = { noremap = true, silent = true }
 
--- Increase window height
 vim.keymap.set('n', '<A-S-Up>', '<cmd>resize +2<CR>', opts)
 vim.keymap.set('n', '<A-S-Down>', '<cmd>resize -2<CR>', opts)
 vim.keymap.set('n', '<A-S-Right>', '<cmd>vertical resize +2<CR>', opts)
 vim.keymap.set('n', '<A-S-Left>', '<cmd>vertical resize -2<CR>', opts)local harpoon = require("harpoon")
 
--- REQUIRED
 harpoon:setup()
--- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>d", function() harpoon:list():remove() end)
@@ -45,30 +41,10 @@ vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
--- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-L>", function() harpoon:list():next() end)
 
 vim.cmd[[highlight WinSeparator guifg=NONE ctermfg=NONE]]
-
-local function adjust_font_size(amount)
-    local font = vim.o.guifont
-    local size = tonumber(font:match("%d+$"))
-    if size then
-        size = size + amount
-        if size > 0 then
-            local new_font = font:gsub("%d+$", tostring(size))
-            vim.o.guifont = new_font
-            print("Font size: " .. size)
-        end
-    end
-end
-
--- Increase font size with '+'
-vim.keymap.set('n', '+', function() adjust_font_size(1) end, { noremap = true, silent = true })
-
--- Decrease font size with '-'
-vim.keymap.set('n', '-', function() adjust_font_size(-1) end, { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {noremap = true, silent = true})
@@ -98,5 +74,5 @@ vim.api.nvim_create_user_command('GoUpdateWorkspace', function()
 end, {})
 
 -- Keybindings for custom commands
-vim.keymap.set('n', '<space>rl', ':RestartLSP<CR>', opts)
-vim.keymap.set('n', '<space>uw', ':GoUpdateWorkspace<CR>', opts)
+vim.keymap.set('n', '<leader>rl', ':RestartLSP<CR>', opts)
+vim.keymap.set('n', '<leader>uw', ':GoUpdateWorkspace<CR>', opts)
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -159,6 +159,5 @@ return {
             require("nvim-tree.api").tree.open()
         end
 
-        vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
     end
 }
