a948ce769fd8b40c3df4b0ad486e0cbc2ad6b53e
Zelferion <zelferionedits@gmail.com>
2025-03-29 19:51:39 -0400

    ...

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,56 +5,51 @@ return {
         dependencies = {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/nvim-cmp",          -- Completion plugin
+            "hrsh7th/cmp-nvim-lsp",      -- LSP source for nvim-cmp
+            "L3MON4D3/LuaSnip",          -- Snippet engine
+            "saadparwaiz1/cmp_luasnip",  -- Snippet completions
         },
         config = function()
+            -- Mason setup
             require("mason").setup()
             require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "pyright",
-                    "ts_ls",
-                    "rust_analyzer",
-                },
+                ensure_installed = { "gopls", "lua_ls", "pyright", "ts_ls", "rust_analyzer" },
                 automatic_installation = true,
             })
 
             local lspconfig = require("lspconfig")
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            local cmp = require("cmp")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+            -- Setup LSP servers
             require("mason-lspconfig").setup_handlers({
-                ["lua_ls"] = function()
-                    lspconfig.lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                semantic = {
-                                    enable = false,
-                                },
-                                diagnostics = {
-                                    globals = {
-                                        "vim",
-                                        "assert",
-                                        "bit",
-                                        "debug",
-                                        "io",
-                                        "jit",
-                                        "math",
-                                        "os",
-                                        "package",
-                                        "string",
-                                        "table",
-                                        "utf8",
-                                    },
-                                },
-                            }
-                        }
+                function(server)
+                    lspconfig[server].setup({
+                        capabilities = capabilities,
                     })
                 end,
-                function(server_name)
-                    if server_name ~= "lua_ls" then
-                        lspconfig[server_name].setup({})
-                    end
-                end,
+            })
+
+            -- Completion setup
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }),
             })
         end,
-    }
+    },
 }
+
